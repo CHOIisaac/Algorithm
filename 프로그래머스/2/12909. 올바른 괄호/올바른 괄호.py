c86bc25a790239384fffe7481ab9fdccd694d948
INGYU CHOI <91372398+CHOIisaac@users.noreply.github.com>
2024-07-14 13:41:18 +0900

    [level 2] Title: 올바른 괄호, Time: 7.54 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,16 @@
+def solution(s):
+    answer = True
+    a = []
+    for x in s:
+        if x == '(':
+            a.append(x)
+        elif x == ')':
+            if a:
+                a.pop()
+            else:
+                answer = False
+    else:
+        if a:
+            answer = False
+
+    return answer
