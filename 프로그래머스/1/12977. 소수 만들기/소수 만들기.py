6c726eb303868c907cdd1d0670ae93f8f35bef4c
INGYU CHOI <91372398+CHOIisaac@users.noreply.github.com>
2024-07-30 23:10:45 +0900

    [level 1] Title: 소수 만들기, Time: 18.14 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -8,12 +8,11 @@ def solution(nums):
         list_.append(sum(c))
     
     for a in list_:
-        flag = True
         for b in range(2, int(math.sqrt(a)+1)):
             if a % b == 0:
                 flag = False
                 break
-        if flag:
+        else:
             answer += 1
     # for i in range(2, int(math.sqrt(sum(combi))+1)):
     #     print(1)
