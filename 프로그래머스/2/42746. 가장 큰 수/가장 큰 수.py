1fa5ec6ac06ebc1bb5edd835ce03558f5b0c6784
INGYU CHOI <91372398+CHOIisaac@users.noreply.github.com>
2024-08-02 23:48:18 +0900

    [level 2] Title: 가장 큰 수, Time: 58.77 ms, Memory: 27.1 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,7 +1,7 @@
 def solution(numbers):
     answer = ''
     nums = list(map(str, numbers))
-    nums.sort(key=lambda x: x*10, reverse=True)
+    nums.sort(key=lambda x: x*3, reverse=True)
     nums_ = ''.join(nums)
     if nums_[0] == '0':
         return '0'
