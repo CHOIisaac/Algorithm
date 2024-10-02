b903f180c8d14db3f2fd94628f833d832be5c73e
INGYU CHOI <91372398+CHOIisaac@users.noreply.github.com>
2024-10-02 10:16:17 +0900

    [level 0] Title: 암호 해독, Time: 0.25 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.py
@@ -1,6 +1,7 @@
 def solution(cipher, code):
     answer = ''
-    for i in range(len(cipher)):
+    # answer = cipher[code-1::code]
+    for i in range(code-1, len(cipher)):
         if (i+1) % code == 0:
             answer += cipher[i]
     return answer
