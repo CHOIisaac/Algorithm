b968d709f29083e769ecc4bc6ff98daf7d6af8a1
INGYU CHOI <91372398+CHOIisaac@users.noreply.github.com>
2024-10-09 22:17:42 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -2,6 +2,5 @@ import re
 
 def solution(my_string):
     answer = 0
-    numbers = re.findall(r'\d+', my_string)
-    answer = sum(map(int, numbers))
-    return answer
+    s = ''.join(i if i.isdigit() else ' ' for i in my_string)
+    return sum(int(i) for i in s.split())
