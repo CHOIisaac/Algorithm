fbd677766cde0d79afcda1c2144939978c98f836
INGYU CHOI <91372398+CHOIisaac@users.noreply.github.com>
2024-10-27 17:59:21 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,18 @@
+def solution(n, lost, reserve):
+    answer = 0
+    lost.sort()
+    reserve.sort()
+    if set(lost) & set(reserve):
+        list_ = set(lost) & set(reserve)
+        for a in list_:
+            lost.remove(a)
+            reserve.remove(a)
+    for i in lost:
+        for j in reserve:
+            if abs(i - j) == 1:
+                answer += 1
+                reserve.remove(j)
+                break
+            
+    answer += n - len(lost)   
+    return answer
