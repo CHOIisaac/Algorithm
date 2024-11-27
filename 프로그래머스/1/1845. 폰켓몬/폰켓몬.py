c130e161966ca6f281d08f7e8d1c1f019542eead
INGYU CHOI <91372398+CHOIisaac@users.noreply.github.com>
2024-11-27 22:01:36 +0900

    [level 1] Title: 폰켓몬, Time: 1.61 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -6,11 +6,7 @@ def solution(nums):
             size_count[size] += 1
         else:
             size_count[size] = 1
-    print(size_count)
     sorted_counts = sorted(size_count.values(), reverse=True)
-    print(sorted_counts)
     you_take = int(sum(sorted_counts)/2)
-    print(you_take)
     answer = min(you_take,len(sorted_counts))
-    print(answer)
     return answer
