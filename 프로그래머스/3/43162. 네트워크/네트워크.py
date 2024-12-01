c7f1e94d793752fa0c85804c315096547364b230
INGYU CHOI <91372398+CHOIisaac@users.noreply.github.com>
2024-12-01 22:18:18 +0900

    [level 3] Title: 네트워크, Time: 0.52 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,19 +1,14 @@
 def solution(n, computers):
     def dfs(node):
-        # 현재 노드 방문 처리
+
         visited[node] = True
-        
-        # 연결된 모든 노드 탐색
         for neighbor in range(n):
             if computers[node][neighbor] == 1 and not visited[neighbor]:
                 dfs(neighbor)
-
-    visited = [False] * n  # 방문 여부 배열
-    network_count = 0      # 네트워크 개수
-    
+    visited = [False] * n  
+    network_count = 0    
     for i in range(n):
-        if not visited[i]:  # 방문하지 않은 컴퓨터에서 DFS 시작
+        if not visited[i]:
             dfs(i)
-            network_count += 1  # 새로운 네트워크 발견
-    
+            network_count += 1
     return network_count
